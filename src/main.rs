fb5aed6972e4eb9c880d139381c595d30f431354
Hans Hazwan <muhammadhazwanzufar79@gmail.com>
2024-08-15 18:20:32 +0800

    Finish Develop the app.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+type Result<T> = core::result::Result<T, Box<dyn std::error::Error>>;
+
+fn main() -> Result<()> {
+    let mut args: Vec<String> = env::args().collect();
+
+    if args.len() == 1 {
+        println!("Hello, World!");
+    } else {
+        args.remove(0);
+
+        let message = args.join(" ");
+
+        println!("Hello {}!", message);
+    }
+
+    Ok(())
 }
